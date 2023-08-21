c8c05221dcc5621b00d9ce5803973095f3f979ee
TheyCallmeOleksii <127785349+TheyCallmeOleksii@users.noreply.github.com>
2023-08-21 05:14:08 +0000

    Deploying to gh-pages from @ TheyCallmeOleksii/goit-js-hw-08@0d473578c844fb20ed1c5e802384d0d0efacfeaf 🚀

--- 02-video.6d7a9e78.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Player(e);var r=throttle((function(e){localStorage.setItem("videoplayer - current - time",e.seconds)}),1e3);t.on("timeupdate",r);var n=localStorage.getItem("videoplayer - current - time");n&&t.setCurrentTime(n).then((function(e){})).catch((function(e){e.name}))}();
-//# sourceMappingURL=02-video.6d7a9e78.js.map
--- 02-video.93ea691d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Player(e);const n=throttle((function(e){localStorage.setItem("videoplayer - current - time",e.seconds)}),1e3);t.on("timeupdate",n);const o=localStorage.getItem("videoplayer - current - time");o&&t.setCurrentTime(o).then((function(e){})).catch((function(e){e.name}));
-//# sourceMappingURL=02-video.93ea691d.js.map
